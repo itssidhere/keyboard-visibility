f2246692f1c0f8f78c9d4e14f8d9cfab8353a9d9
itssidhere <sidjha0001@gmail.com>
2026-06-26 14:38:07 -0600

    stop tracking node_modules and dist
    These should never have been committed. .gitignore now covers both.

--- dist/useKeyboardVisibility.d.ts
+++
@@ -1,6 +0,0 @@
-interface KeyboardVisibility {
-    isKeyboardVisible: boolean;
-    keyboardOffset: number;
-}
-declare const useKeyboardVisibility: () => KeyboardVisibility;
-export default useKeyboardVisibility;
